fb568d767b678ee537a99ecc109db7e7a36668b5
amaar <73219285+amaar-alaa@users.noreply.github.com>
2021-05-17 14:57:23 +0300

    Update settings.py
    dd

--- project/settings.py
+++ project/settings.py
@@ -86,12 +86,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'hr',
+            'USER': 'hruser',
+            'PASSWORD': '123',
+            'HOST': 'localhost',
+            'PORT': '',
+        }}
 
 
 # Password validation
